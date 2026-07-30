d4aad6ceab2f86a7ebd79cc01fef8b73609aadfb
elenafariba <elena800@atomicmail.io>
2026-07-30 22:26:48 +0330

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,6 @@
 {
-  "name": "railway-vless",
+  "name": "railway-vless-proxy",
   "version": "1.0.0",
-  "description": "VLESS WebSocket Proxy for Railway",
   "main": "index.js",
   "scripts": {
     "start": "node index.js"
